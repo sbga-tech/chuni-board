8c5b6dcfb1c8eaa45751a3fbfa9e04a055e1f598
PenguinCaptain <penguin@oshino.moe>
2025-03-18 01:34:13 +0800

    ci: create build workflow

---
+++ release.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    branches: ["main"],
+    plugins: [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        [
+            "@semantic-release/changelog",
+            {
+                changelogFile: "CHANGELOG.md",
+            },
+        ],
+        [
+            "@semantic-release/git",
+            {
+                assets: ["package.json", "CHANGELOG.md"],
+            },
+        ],
+        [
+            "@semantic-release/github",
+            {
+                assets: [
+                    {
+                        path: "dist/chuni-board.exe",
+                    },
+                ],
+            },
+        ],
+    ],
+};
